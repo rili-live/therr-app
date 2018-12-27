dfdf515f2739f64667008a4a5280c324aed11288
Zack Anselm <zack.anselm@blackboard.com>
2018-12-27 11:48:40 -0500

    Console log cleanup

--- rili-server/src/server-socket-io.ts
+++ rili-server/src/server-socket-io.ts
@@ -3,7 +3,7 @@ import * as Redis from 'ioredis';
 import * as socketio from 'socket.io';
 import * as socketioRedis from 'socket.io-redis';
 import * as config from '../config.js';
-import withLogs from 'rili-public-library/utilities/with-logs'; // tslint:disable-line no-implicit-dependencies
+import printLogs from 'rili-public-library/utilities/print-logs'; // tslint:disable-line no-implicit-dependencies
 
 const shouldIncludeLogs = process.argv[2] === 'withLogs';
 const shouldIncludeRedisLogs = process.argv[2] === 'withAllLogs' || process.argv[2] === 'withRedisLogs';
@@ -21,24 +21,20 @@ const nodes = [
     },
 ];
 
-// TODO: RSERV-5: Configure redis clusters
+// TODO: RSERV-6: Configure redis clusters
 // NOTE: Redis cluster only works on Docker for Linux (ie. Ubuntu) using the host network (https://docs.docker.com/network/host/)
 // const redisPubCluster = new Redis.Cluster(nodes);
 // const redisSubCluster = new Redis.Cluster(nodes);
-// EXAMPLE OF PUB/SUB
-// redisSubCluster.on('message', function (channel, message) {
-//     withLogs(shouldIncludeRedisLogs, 'SAMPLE_PUB_SUB' channel, message);
-// });
-
-// redisSubCluster.subscribe('news', function () {
-//     redisPubCluster.publish('news', 'highlights');
-// });
 
 const redisPub = new Redis(nodes[0].port, nodes[0].host, {
+    connectionName: 'redisSocketPub',
     lazyConnect: true,
 });
 
-const redisSub = new Redis(nodes[1].port, nodes[1].host, {
+// TODO: RSERV-5: PubSub doesn't seem to work when on different ports
+// This might simply require redis clusters
+const redisSub = new Redis(nodes[0].port, nodes[0].host, {
+    connectionName: 'redisSocketSub',
     lazyConnect: true,
 });
 
@@ -50,12 +46,7 @@ Promise.all(redisConnectPromises).then((responses: any[]) => {
     if (shouldIncludeRedisLogs) {
         redisPub.monitor().then(function (monitor) {
             monitor.on('monitor', function (time, args, source, database) {
-                withLogs(true, `REDIS_PUB_LOG<time:${time}>`, `Source: ${source}, Database: ${database}`, ...args);
-            });
-        });
-        redisSub.monitor().then(function (monitor) {
-            monitor.on('monitor', function (time, args, source, database) {
-                withLogs(true, `REDIS_SUB_LOG<time:${time}>`, `Source: ${source}, Database: ${database}`, ...args);
+                printLogs(true, `REDIS_PUB_LOG`, time, `Source: ${source}, Database: ${database}`, ...args);
             });
         });
     }
@@ -80,21 +71,29 @@ const startExpressSocketIOServer = () => {
 
     // Redis Error handling
     // redisPubCluster.on('error', (error: string) => {
-    //     withLogs(shouldIncludeRedisLogs, 'REDIS_PUB_CLUSTER_CONNECTION_ERROR:', error);
+    //     printLogs(shouldIncludeRedisLogs, 'REDIS_PUB_CLUSTER_CONNECTION_ERROR:', null, error);
     // });
     // redisSubCluster.on('error', (error: string) => {
-    //     withLogs(shouldIncludeRedisLogs, 'REDIS_SUB_CLUSTER_CONNECTION_ERROR:', error);
-    // });
-    // NOTE: These might be the same as the above error handlers
-    // redisAdapter.pubClient.on('error', (err: string) => {
-    //     withLogs(shouldIncludeRedisLogs, 'PUB_CLIENT_ERROR: ', err);
-    // });
-    // redisAdapter.subClient.on('error', (err: string) => {
-    //     withLogs(shouldIncludeRedisLogs, 'SUB_CLIENT_ERROR: ', err);
+    //     printLogs(shouldIncludeRedisLogs, 'REDIS_SUB_CLUSTER_CONNECTION_ERROR:', null, error);
     // });
 
+    redisAdapter.pubClient.on('error', (err: string) => {
+        printLogs(shouldIncludeRedisLogs, 'REDIS_PUB_CLIENT_ERROR', null, err);
+    });
+    redisAdapter.subClient.on('error', (err: string) => {
+        printLogs(shouldIncludeRedisLogs, 'REDIS_SUB_CLIENT_ERROR', null, err);
+    });
+
+    redisAdapter.subClient.on('subscribe', (channel: any, count: any) => {
+        printLogs(shouldIncludeRedisLogs, 'REDIS_SUB_CLIENT', null, `Subscribed to ${channel}. Now subscribed to ${count} channel(s).`);
+    });
+
+    redisAdapter.subClient.on('message', (channel: any, message: any) => {
+        printLogs(shouldIncludeRedisLogs, 'REDIS_SUB_CLIENT', null, `Message from channel ${channel}: ${message}`); // tslint:disable-line
+    });
+
     io.on('connection', (socket: any) => {
-        withLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', 'NEW CONNECTION...');
+        printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, 'NEW CONNECTION...');
         socket.on('room.join', (details: any) => {
             // Leave all current rooms (except default room) before joining a new one
             Object.keys(socket.rooms)
@@ -107,8 +106,8 @@ const startExpressSocketIOServer = () => {
             setTimeout(() => {
                 socket.join(details.roomName, () => {
                     // TODO: Store the user ID in redis with a username to be used while socket is disconnecting
-                    withLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', `User, ${details.userName} with socketId ${socket.id}, joined room ${details.roomName}`);
-                    withLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', `${details.userName}'s Current Rooms: `, socket.rooms);
+                    printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, `User, ${details.userName} with socketId ${socket.id}, joined room ${details.roomName}`);
+                    printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, `${details.userName}'s Current Rooms: ${JSON.stringify(socket.rooms)}`);
                 });
                 // Emits an event back to the client who joined
                 socket.emit('event', `You joined room ${details.roomName}`);
@@ -118,19 +117,22 @@ const startExpressSocketIOServer = () => {
         });
 
         socket.on('event', (event: any) => {
+            printLogs(shouldIncludeLogs, 'EVENT', null, event);
             if (event.message) {
                 socket.emit('message', `You: ${event.message}`);
                 socket.broadcast.to(event.roomName).emit('message', `${event.userName}: ${event.message}`);
+                printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, `${event.userName} said: ${event.message}`);
             } else {
                 // Broadcasts an event back to the client for all users in the specified room (excluding the user who triggered it)
                 socket.emit('message', `You said hello.`);
                 socket.broadcast.to(event.roomName).emit('message', `${event.userName} says hello!`);
+                printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, `${event.userName} says hello!`);
             }
         });
 
         socket.on('disconnecting', (reason: string) => {
             // TODO: Use the socket ID to retrieve the username from redis
-            withLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', 'DISCONNECTING...', reason);
+            printLogs(shouldIncludeLogs, 'SOCKET_IO_LOGS', null, `DISCONNECTING... ${reason}`);
             Object.keys(socket.rooms)
                 .forEach((room) => {
                     socket.broadcast.to(room).emit('event', `Someone left the room`);
