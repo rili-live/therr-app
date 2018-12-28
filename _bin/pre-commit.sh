d3ead6fc3d2c87412ae354c8edbdcd07dffca43e
Zack Anselm <zack.anselm@blackboard.com>
2018-12-27 22:26:19 -0500

    Updated pre-commit

--- _bin/pre-commit.sh
+++ _bin/pre-commit.sh
@@ -3,9 +3,18 @@ pushd _bin
 source ./lib/colorize.sh
 popd
 
+is_pre_check_success()
+{
+    if [ 1 -eq 1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # TODO: Use CHANGEME.json file to verify development changes and re-build respective pages
 # Also add valid conditions to ensure good commits
-if [ 1 -eq 1 ]; then
+if is_pre_check_success; then
     printMessageNeutral "-- PRE COMMIT SUCCESS --"
 else
     printMessageError "-- PRE COMMIT ERROR --"
