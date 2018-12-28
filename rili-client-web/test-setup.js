c8f8a67feb375bd42862beb8567cd81c999a90ba
Zack Anselm <zack.anselm@blackboard.com>
2018-12-28 15:05:11 -0500

    Added basic test configs

--- rili-client-web/test-setup.js
+++
@@ -1,8 +0,0 @@
-import 'raf/polyfill'; // eslint-disable-line import/no-extraneous-dependencies
-import { configure } from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
-import * as Adapter from 'enzyme-adapter-react-16'; // eslint-disable-line import/no-extraneous-dependencies
-
-Object.defineProperty(document, 'referrer', {
-    value: 'https://www.example.com',
-});
-configure({ adapter: new Adapter() });
