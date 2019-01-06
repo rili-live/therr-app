35fdd9eaf9a4ef0cb967a78011e4a7e5725363f7
Zack Anselm <zack.anselm@blackboard.com>
2019-01-06 13:24:25 -0600

    Added basic styles to web client

--- rili-client-web/src/index.tsx
+++ rili-client-web/src/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app';
 import './styles/index.scss';
-// import 'rili-public-library/styles/index.scss'; // tslint:disable-line no-implicit-dependencies
+import 'rili-public-library/styles/index.scss'; // tslint:disable-line no-implicit-dependencies
 
 ReactDOM.render(<App />, document.getElementById('app'));
