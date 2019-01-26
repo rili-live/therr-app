85806da7dcae898bcc6776a8598798b9de29e584
Zack Anselm <zack.anselm@blackboard.com>
2019-01-26 13:36:10 -0600

    Started implementing web client server side rendering

--- global-config.js
+++ global-config.js
@@ -1,9 +1,11 @@
 module.exports = {
     development: {
-        baseUrl: 'http://localhost:7771'
+        baseUrl: 'http://localhost:7771',
+        clientPort: 7070,
     },
     production: {
-        baseUrl: 'https://rili.live:7743'
+        baseUrl: 'https://rili.live:7743',
+        clientPort: 7070,
     },
     socket: {
     },
