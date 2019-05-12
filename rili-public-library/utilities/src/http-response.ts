27005423bc5ab91b1ec1ba730c81f94a4d9fe711
Zack Anselm <zack.anselm@blackboard.com>
2019-05-12 09:28:26 -0400

    Added http response utilities

---
+++ rili-public-library/utilities/src/http-response.ts
@@ -0,0 +1,20 @@
+import * as httpStatus from 'http-status';
+
+const error = (statusCode: number, error: string) => {
+    return {
+        error,
+        statusCode,
+        status: (httpStatus as any)[statusCode],
+    };
+};
+
+const success = (data: any) => {
+    return {
+        data,
+    };
+};
+
+export {
+    error,
+    success,
+};
