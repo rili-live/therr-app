88e00924967ce582e3bbc8706dc07f53b569489e
Zack Anselm <zanselm@sharpencx.com>
2019-05-26 21:26:34 -0400

    Fixed pm2 mis-configuration

--- rili-server/pm2/ecosystem.config.js
+++ rili-server/pm2/ecosystem.config.js
@@ -9,7 +9,8 @@ module.exports = {
         env_production: {
             NODE_ENV: 'production',
         },
-        args: ['--require=../node_modules/dotenv/config', 'dotenv_config_path=../.env', '--withAllLogs'],
+        args: ['dotenv_config_path=../.env', '--withAllLogs'],
+        node_args: ['--require=../node_modules/dotenv/config'],
         // output: '~/.pm2/logs/rili-server-socket-io-out.log',
         // error: '~/.pm2/logs/rili-server-socket-io-error.log',
         // log: '~/.pm2/logs/rili-server-socket-io-outerr-combined.log',
@@ -23,7 +24,8 @@ module.exports = {
         env_production: {
             NODE_ENV: 'production',
         },
-        args: ['--require=../node_modules/dotenv/config', 'dotenv_config_path=../.env', '--withSQLLogs'],
+        args: ['dotenv_config_path=../.env', '--withSQLLogs'],
+        node_args: ['--require=../node_modules/dotenv/config'],
         // output: '~/.pm2/logs/rili-server-api-out.log',
         // error: '~/.pm2/logs/rili-server-api-error.log',
         // log: '~/.pm2/logs/rili-server-api-outerr-combined.log',
