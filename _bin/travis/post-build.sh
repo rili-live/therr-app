211ffd08af231c3bc3e4ba38fc9f43eb0b240350
Zack Anselm <zanselm@sharpencx.com>
2019-07-13 13:56:04 -0400

    Fixed bin path and updated deps

--- _bin/travis/post-build.sh
+++ _bin/travis/post-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source _bin/lib/git.sh
+source _bin/travis/git.sh
 
 if [ "$LAST_COMMIT_AUTHOR" = "$GIT_AUTHOR_TRAVIS" ]; then
   echo "Previous build committed by git author, $GIT_AUTHOR_TRAVIS. No build changes to commit. Exiting"
