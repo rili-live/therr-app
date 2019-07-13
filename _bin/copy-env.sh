cbb983e3f005400d77260c8b209eb1064a462931
Zack Anselm <zanselm@sharpencx.com>
2019-07-13 17:02:05 -0400

    Fixed syntax

--- _bin/copy-env.sh
+++ _bin/copy-env.sh
@@ -2,15 +2,31 @@
 
 set -e
 
-cp .env.template .env
+if
+  [ [-z ${CLIENT_PORT+x}] || 
+    [-z ${CLIENT_URI+x}] ||
+    [-z ${PG_HOST+x}] ||
+    [-z ${PG_USER+x}] ||
+    [-z ${PG_DATABASE+x}] ||
+    [-z ${PG_PASSWORD+x}] ||
+    [-z ${PG_PORT+x}] ||
+    [-z ${DOMAIN_CERT_LOCATION+x}] ||
+    [-z ${DOMAIN_KEY_LOCATION+x}] ||
+    [-z ${SECRET+x}] ]; then
+  echo "Missing environment variables. copy-env failed."
+  exit 0
+else
+  cp .env.template .env
+  sed -i '' -e "s/CLIENT_PORT=/CLIENT_PORT=$CLIENT_PORT/g" ".env"
+  sed -i '' -e "s/CLIENT_URI=/CLIENT_URI=$CLIENT_URI/g" ".env"
+  sed -i '' -e "s/PG_HOST=/PG_HOST=$PG_HOST/g" ".env"
+  sed -i '' -e "s/PG_USER=/PG_USER=$PG_USER/g" ".env"
+  sed -i '' -e "s/PG_DATABASE=/PG_DATABASE=$PG_DATABASE/g" ".env"
+  sed -i '' -e "s/PG_PASSWORD=/PG_PASSWORD=$PG_PASSWORD/g" ".env"
+  sed -i '' -e "s/PG_PORT=/PG_PORT=$PG_PORT/g" ".env"
+  sed -i '' -e "s/DOMAIN_CERT_LOCATION=/DOMAIN_CERT_LOCATION=$DOMAIN_CERT_LOCATION/g" ".env"
+  sed -i '' -e "s/DOMAIN_KEY_LOCATION=/DOMAIN_KEY_LOCATION=$DOMAIN_KEY_LOCATION/g" ".env"
+  sed -i '' -e "s/SECRET=/SECRET=$SECRET/g" ".env"
+fi
 
-sed -i '' -e "s/CLIENT_PORT=/CLIENT_PORT=$CLIENT_PORT/g" ".env"
-sed -i '' -e "s/CLIENT_URI=/CLIENT_URI=$CLIENT_URI/g" ".env"
-sed -i '' -e "s/PG_HOST=/PG_HOST=$PG_HOST/g" ".env"
-sed -i '' -e "s/PG_USER=/PG_USER=$PG_USER/g" ".env"
-sed -i '' -e "s/PG_DATABASE=/PG_DATABASE=$PG_DATABASE/g" ".env"
-sed -i '' -e "s/PG_PASSWORD=/PG_PASSWORD=$PG_PASSWORD/g" ".env"
-sed -i '' -e "s/PG_PORT=/PG_PORT=$PG_PORT/g" ".env"
-sed -i '' -e "s/DOMAIN_CERT_LOCATION=/DOMAIN_CERT_LOCATION=$DOMAIN_CERT_LOCATION/g" ".env"
-sed -i '' -e "s/DOMAIN_KEY_LOCATION=/DOMAIN_KEY_LOCATION=$DOMAIN_KEY_LOCATION/g" ".env"
-sed -i '' -e "s/SECRET=/SECRET=$SECRET/g" ".env"
+
