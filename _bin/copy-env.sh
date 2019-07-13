d0571869712fb8139864444b4cb5c5a69c8f6cb8
Zack Anselm <zanselm@sharpencx.com>
2019-07-13 17:26:52 -0400

    Fixed tricky syntax

--- _bin/copy-env.sh
+++ _bin/copy-env.sh
@@ -25,17 +25,17 @@ if
   printMessageWarning "Missing environment variables. copy-env failed."
   exit 0
 else
-  cp .env.template .env
-  sed -i '' -e "s/CLIENT_PORT=/CLIENT_PORT=$CLIENT_PORT/g" ".env"
-  sed -i '' -e "s/CLIENT_URI=/CLIENT_URI=$CLIENT_URI/g" ".env"
-  sed -i '' -e "s/PG_HOST=/PG_HOST=$PG_HOST/g" ".env"
-  sed -i '' -e "s/PG_USER=/PG_USER=$PG_USER/g" ".env"
-  sed -i '' -e "s/PG_DATABASE=/PG_DATABASE=$PG_DATABASE/g" ".env"
-  sed -i '' -e "s/PG_PASSWORD=/PG_PASSWORD=$PG_PASSWORD/g" ".env"
-  sed -i '' -e "s/PG_PORT=/PG_PORT=$PG_PORT/g" ".env"
-  sed -i '' -e "s/DOMAIN_CERT_LOCATION=/DOMAIN_CERT_LOCATION=$DOMAIN_CERT_LOCATION/g" ".env"
-  sed -i '' -e "s/DOMAIN_KEY_LOCATION=/DOMAIN_KEY_LOCATION=$DOMAIN_KEY_LOCATION/g" ".env"
-  sed -i '' -e "s/SECRET=/SECRET=$SECRET/g" ".env"
+  cp .env.template .env;
+  find ".env" -type f -exec sed -i '' -e "s/CLIENT_PORT=/CLIENT_PORT=$CLIENT_PORT/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/CLIENT_URI=/CLIENT_URI=$CLIENT_URI/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/PG_HOST=/PG_HOST=$PG_HOST/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/PG_USER=/PG_USER=$PG_USER/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/PG_DATABASE=/PG_DATABASE=$PG_DATABASE/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/PG_PASSWORD=/PG_PASSWORD=$PG_PASSWORD/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/PG_PORT=/PG_PORT=$PG_PORT/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/DOMAIN_CERT_LOCATION=/DOMAIN_CERT_LOCATION=$DOMAIN_CERT_LOCATION/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/DOMAIN_KEY_LOCATION=/DOMAIN_KEY_LOCATION=$DOMAIN_KEY_LOCATION/g" {} \;
+  find ".env" -type f -exec sed -i '' -e "s/SECRET=/SECRET=$SECRET/g" {} \;
   printMessageSuccess "Successfully copied environment variables"
 fi
 
