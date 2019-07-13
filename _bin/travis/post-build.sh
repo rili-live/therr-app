172127c487536a9888056b910c11e77419809dcd
Zack Anselm <zanselm@sharpencx.com>
2019-07-13 16:12:39 -0400

    Updated post build to run on stage and master

--- _bin/travis/post-build.sh
+++ _bin/travis/post-build.sh
@@ -4,7 +4,7 @@ set -e
 
 source _bin/travis/git.sh
 echo "Destination branch is $TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "stage" ]; then
   echo "Skipping post build stage."
   exit 0
 fi
@@ -18,7 +18,7 @@ else
   git add -A
   git commit -a -m "Committed by Travis-CI build number, $((TRAVIS_JOB_ID - 1))" --no-verify
   git remote set-url origin https://${GH_TOKEN}@github.com/rili-live/rili-app.git >/dev/null 2>&1
-  git push origin HEAD:${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} # Push to the source branch before it gets merged to master
+  git push origin HEAD:${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} # Push to the source branch before it gets merged to stage/master
   PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
   git tag $PACKAGE_VERSION
   git push --tags
