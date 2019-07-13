53c43a1588134ee22c7a661076b3325c3943aa2e
Zack Anselm <zanselm@sharpencx.com>
2019-07-13 13:26:05 -0400

    Updated travis file and added scripts

---
+++ _bin/travis/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+if [ "$LAST_COMMIT_AUTHOR" = "$GIT_AUTHOR_TRAVIS" ]; then
+  echo "Previous build committed by git author, $GIT_AUTHOR_TRAVIS. Skipping . Exiting"
+else
+  npm run test:all
+fi
