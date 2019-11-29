6a07fbe0ceb4fc628c4f21e1a69385fe34f1a672
Zack Anselm <zanselm5@gmail.com>
2019-11-29 16:13:23 -0500

    travis yml fixaroo

--- _bin/travis/after-success.sh
+++ _bin/travis/after-success.sh
@@ -8,42 +8,29 @@ source _bin/travis/git.sh
 CURRENT_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-TRAVIS_BRANCH}
 echo "Current branch is $CURRENT_BRANCH"
 echo "Destination branch is $TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  docker build -t rili/nginx -f ./docker/Dockerfile.nginx .
-  docker build -t rili/server-api -f ./rili-server/Dockerfile.api \
-    --build-arg NODE_ENV=production \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  docker build -t rili/server-socket -f ./rili-server/Dockerfile.socket \
-    --build-arg NODE_ENV=production \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  docker build -t rili/client-web -f ./rili-client-web/Dockerfile \
-    --build-arg NODE_ENV=production \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-  docker push rili/nginx
-  docker push rili/server-api
-  docker push rili/server-socket
-  docker push rili/client-web
-elif [ "$TRAVIS_BRANCH" == "stage" ]; then
-  docker build -t rili/nginx-stage -f ./docker/Dockerfile.nginx .
-  docker build -t rili/server-api-stage -f ./rili-server/Dockerfile.api \
-    --build-arg NODE_ENV=stage \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  docker build -t rili/server-socket-stage -f ./rili-server/Dockerfile.socket \
-    --build-arg NODE_ENV=stage \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  docker build -t rili/client-web-stage -f ./rili-client-web/Dockerfile \
-    --build-arg NODE_ENV=stage \
-    --build-arg NODE_VERSION=${NODE_VERSION} .
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-  docker push rili/nginx-stage
-  docker push rili/server-api-stage
-  docker push rili/server-socket-stage
-  docker push rili/client-web-stage
-else
+
+if [[ ("$CURRENT_BRANCH" != "master") && ("$CURRENT_BRANCH" != "stage") ]]; then
   echo "Skipping post build stage."
   exit 0
 fi
+
+[[ "$CURRENT_BRANCH" == "stage" ]] && SUFFIX="-stage" || SUFFIX=""
+
+docker build -t rili/nginx$SUFFIX -f ./docker/Dockerfile.nginx .
+docker build -t rili/server-api$SUFFIX -f ./rili-server/Dockerfile.api \
+  --build-arg NODE_ENV=production \
+  --build-arg NODE_VERSION=${NODE_VERSION} .
+docker build -t rili/server-socket$SUFFIX -f ./rili-server/Dockerfile.socket \
+  --build-arg NODE_ENV=production \
+  --build-arg NODE_VERSION=${NODE_VERSION} .
+docker build -t rili/client-web$SUFFIX -f ./rili-client-web/Dockerfile \
+  --build-arg NODE_ENV=production \
+  --build-arg NODE_VERSION=${NODE_VERSION} .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+docker push rili/nginx$SUFFIX
+docker push rili/server-api$SUFFIX
+docker push rili/server-socket$SUFFIX
+docker push rili/client-web$SUFFIX
 
 
 # if [ "$LAST_COMMIT_AUTHOR" = "$GIT_AUTHOR_TRAVIS" ]; then
