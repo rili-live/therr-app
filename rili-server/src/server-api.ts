7032caea0dc8f7a0a92a87f4cfbea6de8df2e1c6
Zack Anselm <zanselm5@gmail.com>
2019-12-01 11:15:02 -0500

    Elastic beanstalk test one

--- rili-server/src/server-api.ts
+++ rili-server/src/server-api.ts
@@ -4,11 +4,10 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as fs from 'fs';
 import * as https from 'https';
-import * as os from 'os';
 import * as path from 'path';
 import { argv } from 'yargs';
-import * as Knex from 'knex';
 import * as globalConfig from '../../global-config.js';
+import connection from './store/connection';
 import printLogs from 'rili-public-library/utilities/print-logs';
 import AuthRoutes from './api/routes/AuthRoutes';
 import UserRoutes from './api/routes/UserRoutes';
@@ -45,29 +44,9 @@ if (process.env.NODE_ENV !== 'production') {
 // Serves static files in the /build/static directory
 app.use(express.static(path.join(__dirname, 'static')));
 
-// Databse Connection
-const dbConnectionConfig = {
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-};
-
-const knex = Knex({
-    client: 'pg',
-    connection: dbConnectionConfig,
-    pool: {
-        min: 2,
-        max: 10,
-        log: true,
-    },
-    acquireConnectionTimeout: 60000,
-});
-
 // Configure routes
-app.use(API_BASE_ROUTE, (new AuthRoutes(knex)).router);
-app.use(API_BASE_ROUTE, (new UserRoutes(knex)).router);
+app.use(API_BASE_ROUTE, (new AuthRoutes(connection)).router);
+app.use(API_BASE_ROUTE, (new UserRoutes(connection)).router);
 
 if (process.env.NODE_ENV !== 'development') {
     const httpsCredentials = {
