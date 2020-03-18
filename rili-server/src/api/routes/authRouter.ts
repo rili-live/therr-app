bb29dd57df8b3326e87e7b787c3918e114456828
Zack Anselm <zanselm5@gmail.com>
2020-03-18 11:16:24 -0400

    Refactored http responses

--- rili-server/src/api/routes/authRouter.ts
+++ rili-server/src/api/routes/authRouter.ts
@@ -1,7 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import * as express from 'express';
 import * as jwt from 'jsonwebtoken';
-import * as httpResponse from 'rili-public-library/utilities/http-response.js';
 import {
     authenticateUserTokenValidation,
     authenticateUserValidation,
@@ -16,11 +15,6 @@ import UsersStore from '../../store/UsersStore';
 
 const router = express.Router();
 
-const invalidUserNameOrPassword = httpResponse.error({
-    message: 'Incorrect username or password',
-    statusCode: 401,
-});
-
 // Login user
 router.post('/auth', authenticateUserValidation, validate, (req: any, res: any) => UsersStore
     .getUsers({ userName: req.body.userName }, { email: req.body.userName })
@@ -28,40 +22,56 @@ router.post('/auth', authenticateUserValidation, validate, (req: any, res: any) 
         if (!results.length) {
             return res.status(404).send('User not found');
         }
-        bcrypt.compare(req.body.password, results[0].password).then((isValid) => {
-            if (isValid) {
-                const idToken = createUserToken(results[0], req.body.rememberMe);
-                return res.status(201).send(httpResponse.success({
-                    ...results[0],
-                    idToken,
-                }));
-            }
+        return bcrypt.compare(req.body.password, results[0].password)
+            .then((isValid) => [results, isValid]);
+    })
+    .then(([results, isValid]) => {
+        if (isValid) {
+            const idToken = createUserToken(results[0], req.body.rememberMe);
+            return res.status(201).send({
+                ...results[0],
+                idToken,
+            });
+        }
 
-            return res.status(401).send(invalidUserNameOrPassword);
-        }).catch((err: any) => handleHttpError({ err, res, message: 'SQL:AUTH_ROUTES:ERROR' }));
-    }).catch(() => res.status(500).send('something went wrong')));
+        return handleHttpError({
+            res,
+            message: 'Incorrect username or password',
+            statusCode: 401,
+        });
+    })
+    .catch((err) => handleHttpError({ err, res, message: 'SQL:AUTH_ROUTES:ERROR' })));
 
 // Logout user
 router.post('/auth/logout', logoutUserValidation, validate, (req: any, res: any) => UsersStore.getUsers(req.body.userName)
     .then((results) => {
         if (!results.length) {
-            return res.status(404).send('User not found');
+            return handleHttpError({
+                res,
+                message: 'User not found',
+                statusCode: 404,
+            });
         }
         res.status(204).send();
     })
-    .catch(() => res.status(500).send('something went wrong')));
+    .catch((err) => handleHttpError({ err, res, message: 'SQL:AUTH_ROUTES:ERROR' })));
 
 // Validate user token (after login)
 router.post('/auth/user-token/validate', authenticateUserTokenValidation, validate, (req: any, res: any) => {
     try {
         const decodedToken = jwt.verify(req.body.idToken, process.env.SECRET || '');
         return res.status(200).send(decodedToken);
-    } catch (error) {
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).send(error.message);
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            return handleHttpError({
+                res,
+                err,
+                message: err.message,
+                statusCode: 401,
+            });
         }
 
-        return res.status(500).send('something went wrong');
+        return handleHttpError({ err, res, message: 'SQL:AUTH_ROUTES:ERROR' });
     }
 });
 
--- rili-server/src/api/routes/usersRouter.ts
+++ rili-server/src/api/routes/usersRouter.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as httpResponse from 'rili-public-library/utilities/http-response.js';
 import {
     createUserValidation,
 } from '../validation/users';
@@ -8,82 +7,87 @@ import {
 } from '../validation';
 import handleHttpError from '../../utilities/handleHttpError';
 import { hashPassword } from '../../utilities/userHelpers';
-import { HttpErrors } from '../../constants';
 import UsersStore from '../../store/UsersStore';
 
 const router = express.Router();
 
 router.get('/users', (req: any, res: any) => UsersStore.getUsers()
-    .then((results) => res.status(200).send(httpResponse.success(results[0])))
+    .then((results) => res.status(200).send(results[0]))
     .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
 
 router.post('/users', createUserValidation, validate, (req: any, res: any) => UsersStore.findUser(req.body)
     .then((findResults) => {
         if (findResults.length) {
-            return res.status(400).send(httpResponse.error({
-                id: HttpErrors.USER_EXISTS,
+            return handleHttpError({
+                res,
                 message: 'Username, e-mail, and phone number must be unique. A user already exists.',
                 statusCode: 400,
-            }));
+            });
         }
 
         return hashPassword(req.body.password)
-            .then((hash) => UsersStore.createUser({
-                email: req.body.email,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                password: hash,
-                phoneNumber: req.body.phoneNumber,
-                userName: req.body.userName,
-            })
-                .then((results) => res.status(201).send(httpResponse.success(results[0])))
-                .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
+            .then((hash) => UsersStore
+                .createUser({
+                    email: req.body.email,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    password: hash,
+                    phoneNumber: req.body.phoneNumber,
+                    userName: req.body.userName,
+                })
+                .then((results) => res.status(201).send(results[0])));
     })
     .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
 
 router.get('/users/:id', (req, res) => UsersStore.getUsers({ id: req.params.id })
-    .then((results) => res.status(200).send(httpResponse.success(results[0]))).catch((err) => {
-        if (err === 404) {
-            return res.status(404).send(httpResponse.error({
+    .then((results) => {
+        if (!results.length) {
+            return handleHttpError({
+                res,
                 message: `No user found with id, ${req.params.id}.`,
                 statusCode: 404,
-            }));
+            });
         }
-
-        return handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' });
-    }));
+        return res.status(200).send(results[0]);
+    })
+    .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
 
 router.put('/users/:id', (req, res) => UsersStore.findUser({ id: req.params.id, ...req.body })
     .then((findResults) => {
-        if (findResults.length) {
-            return res.status(400).send(httpResponse.error({
-                id: HttpErrors.USER_EXISTS,
+        if (!findResults.length) {
+            return handleHttpError({
+                res,
                 message: 'Username, e-mail, and phone number must be unique. A user already exists.',
                 statusCode: 400,
-            }));
+            });
         }
 
-        return UsersStore.updateUser({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            phoneNumber: req.body.phoneNumber,
-            userName: req.body.userName,
-        }, {
-            id: req.params.id,
-        }).then((results) => res.status(200).send(httpResponse.success(results[0])));
+        return UsersStore
+            .updateUser({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phoneNumber: req.body.phoneNumber,
+                userName: req.body.userName,
+            }, {
+                id: req.params.id,
+            })
+            .then((results) => res.status(200).send(results[0]));
     })
     .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
 
 router.delete('/users/:id', (req, res) => UsersStore.deleteUsers({ id: req.params.id })
     .then((results) => {
-        if (results.length > 0) {
-            return res.status(200).send(httpResponse.success(`User with id, ${req.params.id}, was successfully deleted`));
+        if (!results.length) {
+            return handleHttpError({
+                res,
+                message: `No user found with id, ${req.params.id}.`,
+                statusCode: 404,
+            });
         }
 
-        return res.status(404).send(httpResponse.error({
-            message: `No user found with id, ${req.params.id}.`,
-            statusCode: 404,
-        }));
+        return res.status(200).send({
+            message: `User with id, ${req.params.id}, was successfully deleted`,
+        });
     })
     .catch((err) => handleHttpError({ err, res, message: 'SQL:USER_ROUTES:ERROR' })));
 
