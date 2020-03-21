80f2221207e3cc7ff1263ae81393024a7d58f995
Zack Anselm <zanselm5@gmail.com>
2020-03-21 19:37:27 -0400

    Fixed migration files

--- rili-servers/users-service/src/store/migrations/20190622102513_main.users.js
+++ rili-servers/users-service/src/store/migrations/20190622102513_main.users.js
@@ -1,4 +1,4 @@
-exports.up = (knex) => knex.schema.createTable('main.users', (table) => {
+exports.up = (knex) => knex.schema.withSchema('main').createTable('users', (table) => {
     table.increments('id');
     table.jsonb('accessLevels').defaultTo(JSON.stringify(['user.default']));
     table.string('userName').unique().notNullable();
@@ -7,7 +7,8 @@ exports.up = (knex) => knex.schema.createTable('main.users', (table) => {
     table.string('lastName');
     table.string('password').notNullable();
     table.string('phoneNumber').unique().notNullable();
-    table.timestamps(true, true);
+    table.timestamp('createdAt', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updatedAt', { useTz: true }).notNullable().defaultTo(knex.fn.now());
 });
 
-exports.down = (knex) => knex.schema.dropTable('main.users');
+exports.down = (knex) => knex.schema.withSchema('main').dropTable('users');
