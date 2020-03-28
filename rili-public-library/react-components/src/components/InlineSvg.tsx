fdb6f011fbbd63cc1712eab8de426706e71ca75a
Zack Anselm <zanselm5@gmail.com>
2020-03-28 16:08:48 -0400

    Added tabs to nav menus

--- rili-public-library/react-components/src/components/InlineSvg.tsx
+++ rili-public-library/react-components/src/components/InlineSvg.tsx
@@ -5,7 +5,12 @@ const iconMap: { [index: string]: string } = {
     account: require('../svg-icons/account.svg').toString(), // eslint-disable-line global-require
     close: require('../svg-icons/close.svg').toString(), // eslint-disable-line global-require
     home: require('../svg-icons/home.svg').toString(), // eslint-disable-line global-require
+    location: require('../svg-icons/location.svg').toString(), // eslint-disable-line global-require
     messages: require('../svg-icons/messages.svg').toString(), // eslint-disable-line global-require
+    'notifications-active': require('../svg-icons/notifications-active.svg').toString(), // eslint-disable-line global-require
+    notifications: require('../svg-icons/notifications.svg').toString(), // eslint-disable-line global-require
+    people: require('../svg-icons/people.svg').toString(), // eslint-disable-line global-require
+    settings: require('../svg-icons/settings.svg').toString(), // eslint-disable-line global-require
 };
 
 export interface IInlineSvgProps {
