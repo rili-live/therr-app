1d6152c462cedfee3375a443d443241ade2469e6
Zack Anselm <zanselm5@gmail.com>
2020-03-30 19:11:39 -0400

    Try caching gcloud

--- _bin/travis/before-install.sh
+++ _bin/travis/before-install.sh
@@ -2,8 +2,19 @@
 
 set -e
 
+
 openssl aes-256-cbc -K $encrypted_9f3b5599b056_key -iv $encrypted_9f3b5599b056_iv -in service-account.json.enc -out service-account.json -d
-curl https://sdk.cloud.google.com | bash > /dev/null;
+
+if [ ! -d $HOME/google-cloud-sdk/bin ]; then
+  # The install script errors if this directory already exists,
+  # but Travis already creates it when we mark it as cached.
+  rm -rf $HOME/google-cloud-sdk;
+  # The install script is overly verbose, which sometimes causes
+  # problems on Travis, so ignore stdout.
+  curl https://sdk.cloud.google.com | bash > /dev/null;
+fi
+# This line is critical. We setup the SDK to take precedence in our
+# environment over the old SDK that is already on the machine.
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud components install kubectl
 kubectl version --client
