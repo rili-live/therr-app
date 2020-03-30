3b45ae7d0bed78875c66fa7fb91a4c820357a267
Zack Anselm <zanselm5@gmail.com>
2020-03-30 18:23:55 -0400

    Uncommented deploy scripts

--- _bin/travis/before-install.sh
+++ _bin/travis/before-install.sh
@@ -7,8 +7,8 @@ curl https://sdk.cloud.google.com | bash > /dev/null;
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud components update kubectl
 gcloud auth activate-service-account --key-file service-account.json
-# gcloud config set project rili-kubernetes-261618
-# gcloud config set compute/zone us-central1-a
-# gcloud container clusters get-credentials rili-cluster
+gcloud config set project rili-main
+gcloud config set compute/zone us-central1-a
+gcloud container clusters get-credentials rili-app
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 # docker build -t riliadmin/testing -f ./client/Dockerfile.dev ./client
