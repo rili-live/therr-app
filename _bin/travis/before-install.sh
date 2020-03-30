23c43e01115e6194cf7702cd9d652ec19d21deef
Zack Anselm <zanselm5@gmail.com>
2020-03-30 17:54:46 -0400

    Added encrypted service account file

--- _bin/travis/before-install.sh
+++ _bin/travis/before-install.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# openssl aes-256-cbc -K $encrypted_0c35eebf403c_key -iv $encrypted_0c35eebf403c_iv -in service-account.json.enc -out service-account.json -d
-# curl https://sdk.cloud.google.com | bash > /dev/null;
-# source $HOME/google-cloud-sdk/path.bash.inc
-# gcloud components update kubectl
-# gcloud auth activate-service-account --key-file service-account.json
+openssl aes-256-cbc -K $encrypted_9f3b5599b056_key -iv $encrypted_9f3b5599b056_iv -in service-account.json.enc -out service-account.json -d
+curl https://sdk.cloud.google.com | bash > /dev/null;
+source $HOME/google-cloud-sdk/path.bash.inc
+gcloud components update kubectl
+gcloud auth activate-service-account --key-file service-account.json
 # gcloud config set project rili-kubernetes-261618
 # gcloud config set compute/zone us-central1-a
 # gcloud container clusters get-credentials rili-cluster
