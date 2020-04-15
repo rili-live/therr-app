4739fed6ad19695f175f89ffa9ee8b9f618d1a29
Zack Anselm <zanselm5@gmail.com>
2020-04-15 18:58:15 -0400

    Debug logs

--- rili-servers/websocket-service/src/utilities/restRequest.ts
+++ rili-servers/websocket-service/src/utilities/restRequest.ts
@@ -1,9 +1,23 @@
+import printLogs from 'rili-public-library/utilities/print-logs.js';
 import axios, { AxiosRequestConfig } from 'axios';
+import beeline from '../beeline';
 
-export default (config: AxiosRequestConfig, socket: SocketIO.Socket) => axios({
-    headers: {
-        authorization: `Bearer ${socket.handshake.query.token}`,
-        'x-localecode': socket.handshake.query.localeCode || '',
-    },
-    ...config,
-});
+export default (config: AxiosRequestConfig, socket: SocketIO.Socket) => {
+    printLogs({
+        level: 'debug',
+        messageOrigin: 'SOCKET_REST_REQUEST',
+        messages: [socket.handshake.toString()],
+        tracer: beeline,
+        traceArgs: {
+            socketId: socket.id,
+            socketHandshake: socket.handshake.toString(),
+        },
+    });
+    return axios({
+        headers: {
+            authorization: `Bearer ${socket.handshake.query.token}`,
+            'x-localecode': socket.handshake.query.localeCode || '',
+        },
+        ...config,
+    });
+};
