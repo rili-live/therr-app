76830e00c8928f650a84e3124255513faa7c7289
Zack Anselm <zanselm5@gmail.com>
2020-04-15 20:36:00 -0400

    Fixed the missing env var

--- rili-servers/websocket-service/src/utilities/restRequest.ts
+++ rili-servers/websocket-service/src/utilities/restRequest.ts
@@ -1,23 +1,9 @@
-import printLogs from 'rili-public-library/utilities/print-logs.js';
 import axios, { AxiosRequestConfig } from 'axios';
-import beeline from '../beeline';
 
-export default (config: AxiosRequestConfig, socket: SocketIO.Socket) => {
-    printLogs({
-        level: 'info',
-        messageOrigin: 'SOCKET_REST_REQUEST',
-        messages: 'Socket REST request debug',
-        tracer: beeline,
-        traceArgs: {
-            socketId: socket.id,
-            socketHandshake: JSON.stringify(socket.handshake),
-        },
-    });
-    return axios({
-        headers: {
-            authorization: `Bearer ${socket.handshake.query.token}`,
-            'x-localecode': socket.handshake.query.localeCode || '',
-        },
-        ...config,
-    });
-};
+export default (config: AxiosRequestConfig, socket: SocketIO.Socket) => axios({
+    headers: {
+        authorization: `Bearer ${socket.handshake.query.token}`,
+        'x-localecode': socket.handshake.query.localeCode || '',
+    },
+    ...config,
+});
