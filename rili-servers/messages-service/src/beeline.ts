6733212b15d89d84302f7d2e319f67635543cbaa
Zack Anselm <zanselm5@gmail.com>
2020-05-03 10:29:57 -0400

    Added a messages service

---
+++ rili-servers/messages-service/src/beeline.ts
@@ -0,0 +1,10 @@
+import HoneycombBeeline from 'honeycomb-beeline'; // eslint-disable-line import/newline-after-import
+const beeline = HoneycombBeeline({
+    writeKey: process.env.HONEYCOMB_API_KEY,
+    dataset: 'main',
+    serviceName: 'messages-service',
+
+    /* ... additional optional configuration ... */
+});
+
+export default beeline;
