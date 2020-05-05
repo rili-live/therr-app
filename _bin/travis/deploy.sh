a92cb9d1116e56043b191090f420eccb63003dde
Zack Anselm <zanselm5@gmail.com>
2020-05-05 13:40:29 -0400

    Removed stuff

--- _bin/travis/deploy.sh
+++ _bin/travis/deploy.sh
@@ -18,14 +18,15 @@ fi
 
 [[ "$CURRENT_BRANCH" = "stage" ]] && SUFFIX="-stage" || SUFFIX=""
 
+docker build -t riliadmin/client-web$SUFFIX:latest -t riliadmin/client-web$SUFFIX:$GIT_SHA -f ./rili-client-web/Dockerfile \
+  --build-arg NODE_VERSION=${NODE_VERSION} .
 docker build -t riliadmin/messages-service$SUFFIX:latest -t riliadmin/messages-service$SUFFIX:$GIT_SHA -f ./rili-servers/messages-service/Dockerfile \
   --build-arg NODE_VERSION=${NODE_VERSION} .
 docker build -t riliadmin/users-service$SUFFIX:latest -t riliadmin/users-service$SUFFIX:$GIT_SHA -f ./rili-servers/users-service/Dockerfile \
   --build-arg NODE_VERSION=${NODE_VERSION} .
 docker build -t riliadmin/websocket-service$SUFFIX:latest -t riliadmin/websocket-service$SUFFIX:$GIT_SHA -f ./rili-servers/websocket-service/Dockerfile \
   --build-arg NODE_VERSION=${NODE_VERSION} .
-docker build -t riliadmin/client-web$SUFFIX:latest -t riliadmin/client-web$SUFFIX:$GIT_SHA -f ./rili-client-web/Dockerfile \
-  --build-arg NODE_VERSION=${NODE_VERSION} .
+
 
 docker push riliadmin/client-web$SUFFIX:latest
 docker push riliadmin/client-web$SUFFIX:$GIT_SHA
@@ -37,7 +38,7 @@ docker push riliadmin/websocket-service$SUFFIX:latest
 docker push riliadmin/websocket-service$SUFFIX:$GIT_SHA
 
 kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=riliadmin/client-web:$GIT_SHA
 kubectl set image deployments/messages-service-deployment server=riliadmin/messages-service:$GIT_SHA
 kubectl set image deployments/users-service-deployment server=riliadmin/users-service:$GIT_SHA
 kubectl set image deployments/websocket-service-deployment server=riliadmin/websocket-service:$GIT_SHA
-kubectl set image deployments/client-deployment client=riliadmin/client-web:$GIT_SHA
