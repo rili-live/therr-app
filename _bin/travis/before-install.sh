801c6654d8f8de8244efd242dd0bc5ec16e22eb2
Zack Anselm <zanselm5@gmail.com>
2020-05-05 15:48:12 -0400

    Updated compute/zone

--- _bin/travis/before-install.sh
+++ _bin/travis/before-install.sh
@@ -14,7 +14,7 @@ source $HOME/google-cloud-sdk/path.bash.inc
 gcloud components update kubectl
 gcloud auth activate-service-account --key-file service-account.json
 gcloud config set project rili-main
-gcloud config set compute/zone us-central1-a
+gcloud config set compute/zone us-central1-c
 gcloud container clusters get-credentials rili-cluster
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 # docker build -t riliadmin/testing -f ./client/Dockerfile.dev ./client
