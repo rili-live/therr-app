6bfcb1f07c1bef075f43162bedede413e401d8e0
Zack Anselm <zanselm5@gmail.com>
2020-05-06 12:49:40 -0400

    Revert "Test removing messages service"
    This reverts commit bf2308edcca2d4db2feb1d56cdc583242c72c544.

--- _bin/travis/deploy.sh
+++ _bin/travis/deploy.sh
@@ -20,8 +20,8 @@ fi
 
 docker build -t riliadmin/client-web$SUFFIX:latest -t riliadmin/client-web$SUFFIX:$GIT_SHA -f ./rili-client-web/Dockerfile \
   --build-arg NODE_VERSION=${NODE_VERSION} .
-# docker build -t riliadmin/messages-service$SUFFIX:latest -t riliadmin/messages-service$SUFFIX:$GIT_SHA -f ./rili-servers/messages-service/Dockerfile \
-#   --build-arg NODE_VERSION=${NODE_VERSION} .
+docker build -t riliadmin/messages-service$SUFFIX:latest -t riliadmin/messages-service$SUFFIX:$GIT_SHA -f ./rili-servers/messages-service/Dockerfile \
+  --build-arg NODE_VERSION=${NODE_VERSION} .
 docker build -t riliadmin/users-service$SUFFIX:latest -t riliadmin/users-service$SUFFIX:$GIT_SHA -f ./rili-servers/users-service/Dockerfile \
   --build-arg NODE_VERSION=${NODE_VERSION} .
 docker build -t riliadmin/websocket-service$SUFFIX:latest -t riliadmin/websocket-service$SUFFIX:$GIT_SHA -f ./rili-servers/websocket-service/Dockerfile \
@@ -30,8 +30,8 @@ docker build -t riliadmin/websocket-service$SUFFIX:latest -t riliadmin/websocket
 
 docker push riliadmin/client-web$SUFFIX:latest
 docker push riliadmin/client-web$SUFFIX:$GIT_SHA
-# docker push riliadmin/messages-service$SUFFIX:latest
-# docker push riliadmin/messages-service$SUFFIX:$GIT_SHA
+docker push riliadmin/messages-service$SUFFIX:latest
+docker push riliadmin/messages-service$SUFFIX:$GIT_SHA
 docker push riliadmin/users-service$SUFFIX:latest
 docker push riliadmin/users-service$SUFFIX:$GIT_SHA
 docker push riliadmin/websocket-service$SUFFIX:latest
@@ -39,6 +39,6 @@ docker push riliadmin/websocket-service$SUFFIX:$GIT_SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=riliadmin/client-web:$GIT_SHA
-# kubectl set image deployments/messages-service-deployment server=riliadmin/messages-service:$GIT_SHA
+kubectl set image deployments/messages-service-deployment server=riliadmin/messages-service:$GIT_SHA
 kubectl set image deployments/users-service-deployment server=riliadmin/users-service:$GIT_SHA
 kubectl set image deployments/websocket-service-deployment server=riliadmin/websocket-service:$GIT_SHA
