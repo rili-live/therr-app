66aadbcaf7f03fd426fedcf7f9151d7101b62f23
Zack Anselm <zanselm5@gmail.com>
2020-06-20 19:51:35 -0500

    Added basic logout logic to mobile app

--- RiliMobile/main/routes/Home.tsx
+++ RiliMobile/main/routes/Home.tsx
@@ -8,39 +8,90 @@ import {
     Button,
 } from 'react-native';
 import 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { IUserState } from 'rili-react/types';
 import styles from '../styles';
+import UsersActions from '../redux/actions/UsersActions';
 
-const Home = ({ navigation }) => {
-    return (
-        <>
-            <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}
-                >
-                    <View style={styles.body}>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Home Page</Text>
-                            <Text style={styles.sectionDescription}>
-                                Welcome to the homepage. This is a work in
-                                progress...
-                            </Text>
-                            <Button
-                                color="#1d5b69"
-                                title="Go to Login page"
-                                onPress={() =>
-                                    navigation.navigate('Login', {
-                                        name: 'Login',
-                                    })
-                                }
-                            />
-                        </View>
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        </>
+interface IHomeDispatchProps {
+    login: Function;
+    logout: Function;
+}
+
+interface IStoreProps extends IHomeDispatchProps {
+    user: IUserState;
+}
+
+// Regular component props
+export interface IHomeProps extends IStoreProps {
+    navigation: any;
+}
+
+interface IHomeState {}
+
+const mapStateToProps = (state: any) => ({
+    user: state.user,
+});
+
+const mapDispatchToProps = (dispatch: any) =>
+    bindActionCreators(
+        {
+            login: UsersActions.login,
+            logout: UsersActions.logout,
+        },
+        dispatch
     );
+
+class Home extends React.Component<IHomeProps, IHomeState> {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    handleLogout = () => {
+        const { user, logout, navigation } = this.props;
+
+        logout(user.details)
+            .then(() => {
+                navigation.navigate('Login');
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+    
+    render() {
+        
+
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic"
+                        style={styles.scrollView}
+                    >
+                        <View style={styles.body}>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Home Page</Text>
+                                <Text style={styles.sectionDescription}>
+                                    Welcome to the homepage. This is a work in
+                                    progress...
+                                </Text>
+                                <Button
+                                    color="#1d5b69"
+                                    title="Logout"
+                                    onPress={this.handleLogout}
+                                />
+                            </View>
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
+            </>
+        );
+    }
 };
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
