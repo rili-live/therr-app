f6214fe842f6fcc9a6d912cd368b99eb73c7a03f
Zack Anselm <zanselm5@gmail.com>
2020-09-09 21:07:27 -0500

    Maps service initial commit

---
+++ therr-api-gateway/src/services/maps/router.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+// import * as globalConfig from '../../../../global-config';
+// import handleServiceRequest from '../../middleware/handleServiceRequest';
+// import { validate } from '../../validation';
+
+const mapsServiceRouter = express.Router();
+
+// Maps
+
+export default mapsServiceRouter;
