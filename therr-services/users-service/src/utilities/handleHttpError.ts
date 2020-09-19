d606b4821ac2a147aaf663612cdc8f657cfdd6db
Zack Anselm <zanselm5@gmail.com>
2020-09-19 12:16:03 -0500

    Improved error handler abstraction and debugging

--- therr-services/users-service/src/utilities/handleHttpError.ts
+++ therr-services/users-service/src/utilities/handleHttpError.ts
@@ -1,34 +1,10 @@
-import debug from 'debug';
-import * as express from 'express';
+import { configureHandleHttpError, IErrorArgs } from 'therr-js-utilities/http';
 import beeline from '../beeline';
 
-interface IErrorArgs {
-    err?: Error;
-    res: express.Response;
-    message: string;
-    resBody?: any;
-    statusCode?: number;
-}
-
-const debugHttp = debug('http');
-
-const handleHttpError = ({
-    err,
-    res,
-    message,
-    resBody,
-    statusCode,
-}: IErrorArgs) => {
-    debugHttp((err && err.message) || err);
-    beeline.addContext({
-        errorMessage: err ? err.stack : message,
-    });
-
-    return res.status(statusCode || 500).send({
-        statusCode: statusCode || 500,
-        message,
-        ...resBody,
-    });
+export {
+    IErrorArgs,
 };
+
+const handleHttpError = configureHandleHttpError(beeline);
 
 export default handleHttpError;
