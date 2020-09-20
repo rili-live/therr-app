6e8f0be1ffef01c36cf3cb140da77314ddb1ee2b
Zack Anselm <zanselm5@gmail.com>
2020-09-20 14:55:44 -0500

    Added route to send one time password

--- therr-services/users-service/src/store/migrations/20200919103826_main.users.js
+++ therr-services/users-service/src/store/migrations/20200919103826_main.users.js
@@ -1,7 +1,9 @@
 exports.up = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
+    table.string('onTimePassword');
     table.jsonb('verificationCodes').defaultTo(JSON.stringify({ email: {}, mobile: {} }));
 });
 
 exports.down = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
+    table.dropColumn('onTimePassword');
     table.dropColumn('verificationCodes');
 });
