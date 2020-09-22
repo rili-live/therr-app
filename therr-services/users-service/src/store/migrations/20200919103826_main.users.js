836f362343ccaff4011c6a55a59840acbf053e79
Zack Anselm <zanselm5@gmail.com>
2020-09-21 21:00:44 -0500

    Updated auth route to handle one-time password

--- therr-services/users-service/src/store/migrations/20200919103826_main.users.js
+++ therr-services/users-service/src/store/migrations/20200919103826_main.users.js
@@ -1,9 +1,9 @@
 exports.up = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
-    table.string('onTimePassword');
+    table.string('oneTimePassword');
     table.jsonb('verificationCodes').defaultTo(JSON.stringify({ email: {}, mobile: {} }));
 });
 
 exports.down = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
-    table.dropColumn('onTimePassword');
+    table.dropColumn('oneTimePassword');
     table.dropColumn('verificationCodes');
 });
