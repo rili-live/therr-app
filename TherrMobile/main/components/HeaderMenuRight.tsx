4f7b2f31f29c1dda1c9cff86fd1ccc2289276f76
Zack Anselm <zanselm5@gmail.com>
2020-11-27 21:23:03 -0600

    Fixed location permissions lockout bug

--- TherrMobile/main/components/HeaderMenuRight.tsx
+++ TherrMobile/main/components/HeaderMenuRight.tsx
@@ -4,9 +4,12 @@ import { Button, Overlay, Text } from 'react-native-elements';
 import { CommonActions } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import LocationServicesDialogBox from 'react-native-android-location-services-dialog-box';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 import { headerMenuModal } from '../styles/modal';
+import * as therrTheme from '../styles/themes';
 import translator from '../services/translator';
+import { ILocationState } from '../types/redux/location';
 
 interface IHeaderMenuRightDispatchProps {}
 
@@ -14,10 +17,12 @@ interface IStoreProps extends IHeaderMenuRightDispatchProps {}
 
 // Regular component props
 export interface IHeaderMenuRightProps extends IStoreProps {
+    location: ILocationState;
     logout: Function;
     navigation: any;
     isVisible: boolean;
     isHeaderTransparent: boolean;
+    updateGpsStatus: Function;
     user: any;
 }
 
@@ -50,10 +55,34 @@ class HeaderMenuRight extends React.Component<
     };
 
     navTo = (routeName) => {
-        const { navigation } = this.props;
+        const { location, navigation, updateGpsStatus } = this.props;
 
-        this.toggleOverlay();
-        navigation.navigate(routeName);
+        if (routeName === 'Map' && !location.settings.isGpsEnabled) {
+            LocationServicesDialogBox.checkLocationServicesIsEnabled({
+                message:
+                    "<h2 style='color: #0af13e'>Use Location?</h2>This app wants to change your device settings:<br/><br/>" +
+                    "Use GPS, Wi-Fi, and cell network for location<br/><br/><a href='https://support.google.com/maps/answer/7326816'>Learn more</a>",
+                ok: 'YES',
+                cancel: 'NO',
+                enableHighAccuracy: true, // true => GPS AND NETWORK PROVIDER, false => GPS OR NETWORK PROVIDER
+                showDialog: true, // false => Opens the Location access page directly
+                openLocationServices: true, // false => Directly catch method is called if location services are turned off
+                preventOutSideTouch: false, // true => To prevent the location services window from closing when it is clicked outside
+                preventBackClick: false, // true => To prevent the location services popup from closing when it is clicked back button
+                providerListener: false, // true ==> Trigger locationProviderStatusChange listener when the location state changes
+            })
+                .then((success) => {
+                    updateGpsStatus(success.status);
+                    this.toggleOverlay();
+                    navigation.navigate(routeName);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } else {
+            this.toggleOverlay();
+            navigation.navigate(routeName);
+        }
     };
 
     handleLogout = () => {
@@ -90,7 +119,6 @@ class HeaderMenuRight extends React.Component<
         const { isHeaderTransparent, isVisible, user } = this.props;
         const { isModalVisible } = this.state;
         const currentScreen = this.getCurrentScreen();
-        console.log(currentScreen);
 
         if (isVisible) {
             return (
@@ -122,7 +150,7 @@ class HeaderMenuRight extends React.Component<
                                         <Icon
                                             name="close"
                                             size={30}
-                                            color="black"
+                                            color={therrTheme.colors.primary3}
                                         />
                                     }
                                     onPress={this.toggleOverlay}
