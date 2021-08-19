1dfd369ee5e7b12a65a85bf97e26821b165e573b
Zack Anselm <zanselm5@gmail.com>
2021-08-19 08:24:52 -0500

    Fixed email template

--- therr-services/users-service/src/handlers/subscribers.ts
+++ therr-services/users-service/src/handlers/subscribers.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 import { ErrorCodes } from 'therr-js-utilities/constants';
+import printLogs from 'therr-js-utilities/print-logs';
+import beeline from '../beeline';
 import handleHttpError from '../utilities/handleHttpError';
 import Store from '../store';
 import sendSubscriberVerificationEmail from '../api/email/sendSubscriberVerificationEmail';
@@ -28,9 +30,19 @@ const createSubscriber: RequestHandler = (req: any, res: any) => {
 
             return Store.subscribers.createSubscriber(req.body).then((subscribers) => {
                 sendSubscriberVerificationEmail({
-                    subject: '[Account Created] Therr One-Time Password',
+                    subject: '[Therr] Subscribed to General Updates',
                     toAddresses: [req.body.email],
-                }, {});
+                }, {}).catch((error) => {
+                    printLogs({
+                        level: 'error',
+                        messageOrigin: 'API_SERVER',
+                        messages: [`New subscriber email notification failed for ${req.body.email}`, error?.message],
+                        tracer: beeline,
+                        traceArgs: {
+                            userEmail: req.body.email,
+                        },
+                    });
+                });
 
                 return res.status(201).send(subscribers[0]);
             });
