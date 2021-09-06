2ad3e02e9a2252137e516a143d7f3f192c7d5c41
Zack Anselm <zanselm5@gmail.com>
2021-09-06 13:14:00 -0500

    Updated branch name to main

--- _bin/travis/build.sh
+++ _bin/travis/build.sh
@@ -14,5 +14,5 @@ fi
 if [ "$LAST_COMMIT_AUTHOR" = "$GIT_AUTHOR_TRAVIS" ]; then
   echo "Previous build committed by git author, $GIT_AUTHOR_TRAVIS. Skipping build stage."
 else
-  npm run build:changed ${DESTINATION_BRANCH:-"master"}
+  npm run build:changed ${DESTINATION_BRANCH:-"main"}
 fi
