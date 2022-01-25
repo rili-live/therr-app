aed673790a4573a2cc00491eb1b7b3dff74ea3de
Zack Anselm <rili.main@gmail.com>
2022-01-25 07:44:51 -0600

    fix: build issue resolution

--- TherrMobile/android/app/src/main/java/app/therrmobile/MainApplication.java
+++ TherrMobile/android/app/src/main/java/app/therrmobile/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.reactnativecommunity.picker.RNCPickerPackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
