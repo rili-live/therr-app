da2a68df63f3112c445823745427e84e8f293cf5
rob <imafishyfriend@gmail.com>
2022-05-04 17:01:19 -0500

    styled tiles and added image links

--- therr-client-web/src/routes/Discovered/Tile.tsx
+++ therr-client-web/src/routes/Discovered/Tile.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from 'axios'
 
 interface MomentProps {
     moment: {
         id: string,
         fromUserName: string,
-        message: string
+        message: string,
+        media: [{
+            id: string
+        }],
     }
 }
 
@@ -12,16 +17,19 @@ const Tile: React.FC<MomentProps> = ({ moment }) => {
     
     const [liked, setLiked] = useState(false);
 
+    const content: any = useSelector((state: any) => state.content)
+
     const handleLike = () => {
         setLiked(!liked);
     }
 
     return <div className="tile">
             <p className="tile_username">{moment.fromUserName}</p>
-            <img 
+            {moment.media.length > 0 && <img 
                 className="tile_image" 
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" 
-                alt={moment.fromUserName} />
+                // src={content.media[moment.media[0].id]}
+                src={content.media[moment.media[0].id]}
+                alt={moment.fromUserName} />}
             <div className='tile_lower_content'>
                 <p className="tile_message">{moment.message}</p>
                 <button className={liked ? "tile_button_liked" : 'tile_button_unliked'} onClick={handleLike}>❤️</button>
