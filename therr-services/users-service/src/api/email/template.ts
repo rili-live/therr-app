37f0c46ce34dc8b1f139bbab377dd4d1038fb5f7
Zack Anselm <rili.main@gmail.com>
2022-07-10 16:05:58 -0500

    feat: updated email template

--- therr-services/users-service/src/api/email/template.ts
+++ therr-services/users-service/src/api/email/template.ts
@@ -57,14 +57,14 @@ const template = `
                       <p style="font-family: sans-serif; font-size: 14px; font-weight: bold; margin: 0; Margin-bottom: 15px;">{{bodyBold}}</p>
                       {{/if}}
                       {{#if buttonHref}}
-                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
-                          <tbody>
-                            <tr>
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
+                          <tbody align="center">
+                            <tr align="center">
                               <td align="center" style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;">
-                                <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
-                                  <tbody>
-                                    <tr>
-                                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #2692c5; border-radius: 5px; text-align: center;"> <a href="{{buttonHref}}" target="_blank" style="display: inline-block; letter-spacing: .05rem; color: #ffffff; background-color: #2692c5; border: solid 1px #2692c5; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">{{buttonText}}</a> </td>
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
+                                  <tbody align="center">
+                                    <tr align="center">
+                                      <td align="center" style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #2692c5; border-radius: 5px; text-align: center;"> <a href="{{buttonHref}}" target="_blank" style="display: inline-block; letter-spacing: .05rem; color: #ffffff; background-color: #2692c5; border: solid 1px #2692c5; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">{{buttonText}}</a> </td>
                                     </tr>
                                   </tbody>
                                 </table>
