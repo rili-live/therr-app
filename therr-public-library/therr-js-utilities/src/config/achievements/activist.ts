de4a32ecf629ca5d669b86dc6525732a579b1186
Zack Anselm <rili.main@gmail.com>
2022-07-31 17:06:24 -0500

    feat: add migrations and endpoints for userAchievements

---
+++ therr-public-library/therr-js-utilities/src/config/achievements/activist.ts
@@ -0,0 +1,19 @@
+// IMPORTANT: Developers should update version after making any changes to these configs. An achievement key/id should never be changed
+// NOTE: title and description should be translated by the consumer of this config
+// using translation keys (ex. explorer_1_1.title, explorer_1_1.description)
+
+const activistAchievements = {
+    activist_1_1: {
+        title: '',
+        description: '',
+        bonusAbilityId: '',
+        prerequisite: (userAchievements: { [key: string]: any }) => true,
+        countToComplete: 0,
+        xp: 1,
+        pointReward: 1.00,
+        mediaId: '',
+        version: 1,
+    },
+};
+
+export default activistAchievements;
