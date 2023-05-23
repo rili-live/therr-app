8e3fb0120f52e88cfaa10ed6cd68682f15dd8ea9
Zack Anselm <rili.main@gmail.com>
2023-05-23 15:38:35 -0500

    fix: update email validator

--- therr-public-library/therr-js-utilities/src/email-validator.ts
+++ therr-public-library/therr-js-utilities/src/email-validator.ts
@@ -2,7 +2,7 @@ import emailValidator from 'email-validator';
 
 export default class CustomEmailValidator {
     public static validate(email: string): boolean {
-        if (email.endsWith('.vom') || email.endsWith('gmaol.com') || email.endsWith('sil.com')) {
+        if (email.endsWith('.vom') || email.endsWith('gmaol.com') || email.endsWith('sil.com') || email.endsWith('1secmail.org')) {
             return false;
         }
         return emailValidator.validate(email);
