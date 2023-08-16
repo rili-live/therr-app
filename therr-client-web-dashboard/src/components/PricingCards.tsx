6835518446246af4bc0af3f6ed1e1c9d495d8be2
Zack Anselm <rili.main@gmail.com>
2023-08-16 10:51:15 -0500

    feat: update pricing cards

--- therr-client-web-dashboard/src/components/PricingCards.tsx
+++ therr-client-web-dashboard/src/components/PricingCards.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import ReactGA from 'react-ga4';
 import {
@@ -30,7 +31,7 @@ const PricingCards = ({
 
     return (
         <Row className="d-flex justify-content-around align-items-center py-4">
-            <Col xs={12} xl={10} xxl={8}>
+            <Col xs={12} xl={12} xxl={10}>
                 <Card>
                     <Card.Body>
                         <Row className="flex-row justify-content-center align-items-center">
@@ -40,42 +41,86 @@ const PricingCards = ({
                         </Row>
                         <Row className="flex-row justify-content-center align-items-center">
                             <Col xs={12} xl={6} xxl={4} className="text-center">
-                                <Row className="flex-column justify-content-center align-items-center mx-2 mb-2">
-                                    <Button
-                                        onClick={getOnClickUpgrade('basic')}
-                                        href={'https://buy.stripe.com/3cs7tkcsZ6z4fTy7ss'}
-                                        target="_blank"
-                                        variant="secondary"
-                                    >
-                                        <FontAwesomeIcon icon={faRocket} className="me-1" />
-                                        Upgrade to Basic ($14.99)
-                                    </Button>
+                                <Row className="justify-content-center align-items-center mx-2 mb-4">
+                                    <Card className="bg-basic-card pricing-card-basic">
+                                        <Card.Body>
+                                            <h3 className="text-white">Basic Marketing Plan</h3>
+                                            <h6 className="text-white">Benefit from our basic local business marketing support package along with metrics from current and prospective customers.</h6>
+                                            <hr />
+                                            <Button
+                                                onClick={getOnClickUpgrade('basic')}
+                                                href={'https://buy.stripe.com/3cs7tkcsZ6z4fTy7ss'}
+                                                target="_blank"
+                                                variant="primary"
+                                                className="text-white"
+                                            >
+                                                <FontAwesomeIcon icon={faRocket} className="me-1" />
+                                                Upgrade to Basic ($14.99)
+                                            </Button>
+                                            <hr />
+                                            <ul className="list-unstyled text-white text-left">
+                                                <li><i className="fas fa-check"></i> Business space prioritized on mobile app</li>
+                                                <li><i className="fas fa-check"></i> Additional customer metrics</li>
+                                            </ul>
+                                        </Card.Body>
+                                    </Card>
                                 </Row>
                             </Col>
                             <Col xs={12} xl={6} xxl={4} className="text-center">
-                                <Row className="flex-column justify-content-center align-items-center mx-2 mb-2">
-                                    <Button
-                                        onClick={getOnClickUpgrade('advanced')}
-                                        href={'https://buy.stripe.com/aEUdRI78F0aGePu6op'}
-                                        target="_blank"
-                                        variant="secondary"
-                                    >
-                                        <FontAwesomeIcon icon={faRocket} className="me-1" />
-                                        Upgrade to Advanced ($34.99)
-                                    </Button>
+                                <Row className="justify-content-center align-items-center mx-2 mb-4">
+                                    <Card className="bg-advanced-card pricing-card-advanced">
+                                        <Card.Body>
+                                            <h3 className="text-white">Advanced Marketing Plan</h3>
+                                            <h6 className="text-white">Benefit from our premium local business marketing support package along with advanced metrics from current and prospective customers.</h6>
+                                            <hr />
+                                            <Button
+                                                onClick={getOnClickUpgrade('advanced')}
+                                                href={'https://buy.stripe.com/aEUdRI78F0aGePu6op'}
+                                                target="_blank"
+                                                variant="primary"
+                                                className="text-white"
+                                            >
+                                                <FontAwesomeIcon icon={faRocket} className="me-1" />
+                                                Upgrade to Advanced ($34.99)
+                                            </Button>
+                                            <hr />
+                                            <ul className="list-unstyled text-white text-left">
+                                                <li><i className="fas fa-check"></i> Business space prioritized on mobile app</li>
+                                                <li><i className="fas fa-check"></i> Additional customer metrics</li>
+                                                <li><i className="fas fa-check"></i> Customer & Micro-influencer incentives (for reviews, shout-outs, promos, etc.)</li>
+                                            </ul>
+                                        </Card.Body>
+                                    </Card>
                                 </Row>
                             </Col>
                             <Col xs={12} xl={6} xxl={4} className="text-center">
-                                <Row className="flex-column justify-content-center align-items-center mx-2 mb-2">
-                                    <Button
-                                        onClick={getOnClickUpgrade('pro')}
-                                        href={'https://buy.stripe.com/8wM14W64Bg9E36M146'}
-                                        target="_blank"
-                                        variant="secondary"
-                                    >
-                                        <FontAwesomeIcon icon={faRocket} className="me-1" />
-                                        Upgrade to Pro ($99.99)
-                                    </Button>
+                                <Row className="justify-content-center align-items-center mx-2 mb-4">
+                                    <Card className="bg-pro-card pricing-card-pro">
+                                        <Card.Body>
+                                            <h3 className="text-white">Pro Marketing Plan</h3>
+                                            <h6 className="text-white">Benefit from our professional local business marketing support package along with high fidelity metrics from current and prospective customers.</h6>
+                                            <hr />
+                                            <Button
+                                                onClick={getOnClickUpgrade('pro')}
+                                                href={'https://buy.stripe.com/8wM14W64Bg9E36M146'}
+                                                target="_blank"
+                                                variant="primary"
+                                                className="text-white"
+                                            >
+                                                <FontAwesomeIcon icon={faRocket} className="me-1" />
+                                                Upgrade to Pro ($99.99)
+                                            </Button>
+                                            <hr />
+                                            <ul className="list-unstyled text-white text-left">
+                                                <li><i className="fas fa-check"></i> Business space prioritized on mobile app</li>
+                                                <li><i className="fas fa-check"></i> Additional customer metrics</li>
+                                                <li><i className="fas fa-check"></i> Customer & influencer incentives (for reviews, shout-outs, promos, etc.)</li>
+                                                <li><i className="fas fa-check"></i> Point-of-purchase incentives for customers & influencers</li>
+                                                <li><i className="fas fa-check"></i> Influencer marketing on Instagram/Twitter/etc.</li>
+                                                <li><i className="fas fa-check"></i> Curated Google Ads campaign</li>
+                                            </ul>
+                                        </Card.Body>
+                                    </Card>
                                 </Row>
                             </Col>
                         </Row>
