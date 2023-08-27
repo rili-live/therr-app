8d8a381aa20c78f2e5a3cd8b2cc1b96bbfafa4d0
samroadie <somyadeep99@gmail.com>
2023-08-26 22:54:19 -0700

    Rating Test

--- therr-services/reactions-service/tests/unit/SpaceReactionsStore.test.ts
+++ therr-services/reactions-service/tests/unit/SpaceReactionsStore.test.ts
@@ -1,36 +1,30 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import SpaceReactionsStore, { SPACE_REACTIONS_TABLE_NAME } from '../../src/store/SpaceReactionsStore';
+import SpaceReactionsStore from '../../src/store/SpaceReactionsStore';
 
 describe('SpaceReactionsStore', () => {
     describe('getRatingsBySpaceId', () => {
         it('queries for ratings with default limit', async () => {
-            const expectedQuery = `select "rating" from "${SPACE_REACTIONS_TABLE_NAME}" where "rating" is not null limit 1000`; // Adjust based on actual SQL query
+            const expectedQuery = `select "rating" from "main"."spaceReactions" where "rating" is not null limit 1000`;
             const mockStore = {
                 read: {
                     query: sinon.stub().callsFake(() => Promise.resolve({})),
                 },
             };
-            
             const store = new SpaceReactionsStore(mockStore);
             store.getRatingsBySpaceId({ someCondition: true });
-            
-            expect(mockStore.read.query.calledOnceWith(expectedQuery)).to.be.true;
+            expect(mockStore.read.query.args[0][0]).to.be.equal(expectedQuery);
         });
-
         it('restricts the limit to 5000', async () => {
-            const expectedQuery = `select "rating" from "${SPACE_REACTIONS_TABLE_NAME}" where "rating" is not null limit 5000`;
+            const expectedQuery = `select "rating" from "main"."spaceReactions" where "rating" is not null limit 5000`;
             const mockStore = {
                 read: {
                     query: sinon.stub().callsFake(() => Promise.resolve({})),
                 },
             };
-            
             const store = new SpaceReactionsStore(mockStore);
-            await store.getRatingsBySpaceId({}, 6000);
-            
-            expect(mockStore.read.query.calledOnceWith(expectedQuery)).to.be.true;
+            store.getRatingsBySpaceId({}, 6000);
+            expect(mockStore.read.query.args[0][0]).to.be.equal(expectedQuery);
         });
-
     });
 });
