b61b347899be181816b38a435f644168084b9f96
Zack Anselm <zanselm5@gmail.com>
2023-09-04 07:57:41 -0500

    feat: implement opentelemetry on maps-service

---
+++ therr-services/maps-service/src/tracing.ts
@@ -0,0 +1,19 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { HoneycombSDK } from '@honeycombio/opentelemetry-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+
+// Uses environment variables named HONEYCOMB_API_KEY and OTEL_SERVICE_NAME
+const sdk: NodeSDK = new HoneycombSDK({
+    serviceName: 'maps-service',
+    instrumentations: [
+        getNodeAutoInstrumentations({
+            // We recommend disabling fs automatic instrumentation because
+            // it can be noisy and expensive during startup
+            '@opentelemetry/instrumentation-fs': {
+                enabled: false,
+            },
+        }),
+    ],
+});
+
+export default sdk;
