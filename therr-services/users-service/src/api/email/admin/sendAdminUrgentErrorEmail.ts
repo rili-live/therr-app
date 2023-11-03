77df6becde7ebed0368d47dbc842bbac48bd77a4
Zack Anselm <rili.main@gmail.com>
2023-11-03 14:05:23 -0500

    feat: update user access level after subscription

---
+++ therr-services/users-service/src/api/email/admin/sendAdminUrgentErrorEmail.ts
@@ -0,0 +1,39 @@
+/* eslint-disable max-len */
+import Handlebars from 'handlebars';
+import sendEmail from '../sendEmail';
+import templateString from '../template';
+
+export interface ISendAdminNewBusinessSubscriptionEmailConfig {
+    charset?: string;
+    subject: string;
+    toAddresses: string[];
+}
+
+export interface ITemplateParams {
+    errorMessage: string;
+}
+
+export interface IAccountTypeParams {
+    webhookEventType?: string,
+    webhookEventAmount?: number,
+    webhookEventStatus?: string,
+}
+
+export default (emailParams: ISendAdminNewBusinessSubscriptionEmailConfig, templateParams: ITemplateParams, errorDetails: IAccountTypeParams) => {
+    const otherEmails = (process.env.AWS_FEEDBACK_EMAIL_ADDRESS || '').split(',');
+    const template = Handlebars.compile(templateString);
+    const dearUser = 'Oops something went wrong!';
+    const htmlConfig = {
+        header: 'This is an urgent error',
+        dearUser,
+        body1: `ErrorMessage: ${templateParams.errorMessage}`,
+        body2: `AdditionalDetails: ${JSON.stringify(errorDetails)}`,
+    };
+    const html = template(htmlConfig);
+
+    return sendEmail({
+        ...emailParams,
+        html,
+        toAddresses: [...emailParams.toAddresses, ...otherEmails],
+    });
+};
--- therr-services/users-service/src/api/email/for-business/sendDashboardSubscriberIntroEmail.ts
+++ therr-services/users-service/src/api/email/for-business/sendDashboardSubscriberIntroEmail.ts
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 import Handlebars from 'handlebars';
-import sendEmail from './sendEmail';
-import * as globalConfig from '../../../../../global-config';
-import templateString from './template';
+import sendEmail from '../sendEmail';
+import * as globalConfig from '../../../../../../global-config';
+import templateString from '../template';
 
 export interface ISendDashboardSubscriberIntroEmailConfig {
     charset?: string;
@@ -23,8 +23,9 @@ export default (emailParams: ISendDashboardSubscriberIntroEmailConfig, templateP
         header: 'Therr For Business: Marketing & Metrics Plan',
         dearUser: 'Welcome!',
         body1: `Your business is now activated for "${productPlanName}", and your subscription will auto-renew at the end of the free trial. If you already have a dashboard account, login to manage your business space.`,
-        body2: 'Otherwise, follow the link to register and get started. Claim your space and update your business details for the best results from our marketing campaigns catered directly to your unique business needs.',
+        body2: 'Otherwise, follow the link to register and get started. Claim your space and update your business details for the best results. Our marketing campaigns cater directly to your unique business needs with advance AI and automation.',
         body3: `If you have questions about the process or wish to update your plan, feel free to contact support at any time: ${process.env.AWS_FEEDBACK_EMAIL_ADDRESS}`,
+        bodyBold: 'You may need to logout and log back in to see the updated account access.',
         buttonHref: linkUrl,
         buttonText: 'Login or Register',
         postBody1: `If you are unable to click the link, copy paste the following URL in the browser: ${linkUrl}`,
