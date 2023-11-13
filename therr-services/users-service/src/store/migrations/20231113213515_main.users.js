b80e82d2a29a418ffa906bcd78dae77522aef4d4
Zack Anselm <rili.main@gmail.com>
2023-11-13 15:57:34 -0600

    feat: add index

--- therr-services/users-service/src/store/migrations/20231113213515_main.users.js
+++ therr-services/users-service/src/store/migrations/20231113213515_main.users.js
@@ -1,5 +1,7 @@
 exports.up = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
     table.string('billingEmail');
+
+    table.index('billingEmail').index('phoneNumber');
 });
 
 exports.down = (knex) => knex.schema.withSchema('main').alterTable('users', (table) => {
