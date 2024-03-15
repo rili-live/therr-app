54fd83f1824453274e9ae1bcc47d5112eccdf848
Zack Anselm <rili.main@gmail.com>
2024-03-15 13:12:30 -0500

    fix: space cache invalidation

--- therr-public-library/therr-react/src/redux/actions/Maps.ts
+++ therr-public-library/therr-react/src/redux/actions/Maps.ts
@@ -19,6 +19,7 @@ export type IEngagementTypes = 'check-in' | 'moment' | 'event';
 
 const Maps = {
     // Media
+    // TODO: Accept device dimensions and optimize image size when response contains imageKit urls
     fetchMedia: (mediaIds: string[]) => (dispatch: any) => MapsService.fetchMedia(mediaIds).then((response: any) => {
         dispatch({
             type: ContentActionTypes.FETCH_MEDIA,
