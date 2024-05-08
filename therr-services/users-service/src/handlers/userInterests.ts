72a4b5c101e4894d1a61b58f6f0c29225a2b5135
Zack Anselm <zanselm5@gmail.com>
2024-05-08 10:03:12 -0500

    fix: user interests response

--- therr-services/users-service/src/handlers/userInterests.ts
+++ therr-services/users-service/src/handlers/userInterests.ts
@@ -55,13 +55,8 @@ const getUserInterests = (req, res) => {
 
     const contextUserId = isMeRoute ? userId : req.params.userId || userId;
 
-    const {
-        startDate,
-        endDate,
-    } = req.query;
-
     return Store.userInterests.getByUserId(contextUserId)
-        .then((results) => res.status(results))
+        .then((results) => res.status(200).send(results))
         .catch((err) => handleHttpError({
             err,
             res,
