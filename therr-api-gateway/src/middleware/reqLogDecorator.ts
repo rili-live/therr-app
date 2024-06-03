0c6730c2ebd6b5785c31b1e915dcbd2b5895438d
Zack Anselm <zanselm5@gmail.com>
2024-06-03 09:06:24 -0500

    feat: platform

--- therr-api-gateway/src/middleware/reqLogDecorator.ts
+++ therr-api-gateway/src/middleware/reqLogDecorator.ts
@@ -6,6 +6,7 @@ export default (req, res, next) => {
     const activeSpan = opentelemetry.trace.getActiveSpan();
     activeSpan?.setAttribute('request.headers.userid', req.headers?.['x-userid']);
     activeSpan?.setAttribute('request.headers.authorization', req.headers?.authorization ? 'XX_REDACTED_XX' : '');
+    activeSpan?.setAttribute('request.headers.platform', req.headers?.['x-platform']);
     activeSpan?.setAttribute('request.app', req.app);
     activeSpan?.setAttribute('request.ip', req.ip);
     activeSpan?.setAttribute('request.osHostname', os.hostname());
