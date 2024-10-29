8f2c6d6f06affd279a84366c17446619fafd05b5
Zack Anselm <zanselm5@gmail.com>
2024-10-29 15:31:36 -0500

    feat: added an app logging endpoint and logs to debug notifications

--- therr-public-library/therr-react/src/services/MessagesService.ts
+++ therr-public-library/therr-react/src/services/MessagesService.ts
@@ -1,9 +1,20 @@
 /* eslint-disable class-methods-use-this */
 import axios from 'axios';
+import { ILogLevel } from 'therr-js-utilities/constants';
 import { getSearchQueryString } from 'therr-js-utilities/http';
 import { ISearchQuery } from '../types';
 
 class MessagesService {
+    sendAppLog = (data: { [key: string]: string }, logLevel?: ILogLevel) => {
+        const url = `/messages-service/app-logs?logLevel=${logLevel || 'info'}`;
+
+        return axios({
+            method: 'post',
+            url,
+            data,
+        }).catch((err) => console.log(`Failed to send app log likely due to bad connection: ${err.message}`));
+    };
+
     searchDMs = (query: ISearchQuery) => {
         let queryString = getSearchQueryString(query);
 
